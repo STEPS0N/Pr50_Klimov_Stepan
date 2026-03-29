dfe16901d31c57ba44e4535be94ea186ee2a4d2b
STEPS0N <stepan.klimoff@mail.ru>
2026-03-29 19:16:48 +0500

    Разработанный пользовательский элемент для клиента. Климов

---
+++ AppKeyPass/Elements/Item.xaml.cs
@@ -0,0 +1,52 @@
+﻿using AppKeyPass.Models;
+using AppKeyPass.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AppKeyPass.Elements
+{
+    /// <summary>
+    /// Логика взаимодействия для Item.xaml
+    /// </summary>
+    public partial class Item : UserControl
+    {
+        Storage Storage;
+        Main Main;
+
+        public Item(Storage storage, Main main)
+        {
+            InitializeComponent();
+            tbName.Text = storage.Name;
+            tbUrl.Text = storage.Url;
+            tbLogin.Text = storage.Login;
+            tbPassword.Text = storage.Password;
+
+            this.Storage = storage;
+            this.Main = main;
+        }
+
+        private void Update(object sender, RoutedEventArgs e)
+        {
+            MainWindow.init.OpenPage(new Pages.Add(Storage));
+        }
+
+        private void Delete(object sender, RoutedEventArgs e)
+        {
+            StorageContext.Delete(Storage.Id);
+            this.Main.StorageList.Children.Remove(this);
+            MessageBox.Show("Данные удалены");
+        }
+    }
+}
