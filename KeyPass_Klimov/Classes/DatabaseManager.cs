fcbd8c7657c1b39a5a3fb9c41bc2f0f5fd114208
STEPS0N <stepan.klimoff@mail.ru>
2026-03-29 16:51:37 +0500

    Разработанные вспомогательные классы для сервера. Климов

---
+++ KeyPass_Klimov/Classes/DatabaseManager.cs
@@ -0,0 +1,22 @@
+﻿using KeyPass_Klimov.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace KeyPass_Klimov.Classes
+{
+    public class DatabaseManager : DbContext
+    {
+        public DbSet<Storage> Storages { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        public DatabaseManager()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("server=localhost;port=3307;uid=root;pwd=;database=Storage;",
+                new MySqlServerVersion(new Version(8, 0, 11)));
+        }
+    }
+}
---
+++ KeyPass_Klimov/Classes/JwtToken.cs
@@ -0,0 +1,73 @@
+﻿using KeyPass_Klimov.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace KeyPass_Klimov.Classes
+{
+    /// <summary>
+    /// Отвечает за генерацию и валидацию токенов
+    /// </summary>
+    public class JwtToken
+    {
+        /// <summary>
+        /// Секретный ключ для подписи токенов
+        /// static означает, что ключ общий для всех экзэмпляров класса
+        /// </summary>
+        static byte[] Key = Encoding.UTF8.GetBytes("");
+
+        /// <summary>
+        /// Генерирует JWT токен для пользователя
+        /// </summary>
+        /// <param name="user">Пользователь, для которого создаётся токен</param>
+        /// <returns>Строка с JWT токеном</returns>
+        public static string Generate(User user)
+        {
+            JwtSecurityTokenHandler TokenHandler = new JwtSecurityTokenHandler();
+
+            SecurityTokenDescriptor tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim("UserId", user.Id.ToString())
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(Key),
+                    SecurityAlgorithms.HmacSha256Signature
+                )
+            };
+            SecurityToken Token = TokenHandler.CreateToken(tokenDescriptor);
+            return TokenHandler.WriteToken(Token);
+        }
+
+        /// <summary>
+        /// Извлекает ID пользователя из JWT токена
+        /// </summary>
+        /// <param name="token">JWT токен в виде строки</param>
+        /// <returns>ID пользователя или null, если токен недействителен</returns>
+        public static int? GetUserIdFromToken(string token)
+        {
+            try
+            {
+                JwtSecurityTokenHandler TokenHandler = new JwtSecurityTokenHandler();
+                TokenHandler.ValidateToken(token, new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    ClockSkew = TimeSpan.Zero
+                }, out SecurityToken ValidatedToken);
+                JwtSecurityToken JwtToken = (JwtSecurityToken)ValidatedToken;
+                string UserId = JwtToken.Claims.First(x => x.Type == "UserId").Value;
+                return int.Parse(UserId);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
